4c9a58d46c808f8c7b6fba68b4e74fedcb5d3777
Twahidul Islam <twahidulislam2005@gmail.com>
2025-11-03 20:52:50 +0600

    Refactor code structure for improved readability and maintainability

--- src/components/CategoriesCard.jsx
+++ src/components/CategoriesCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CategoriesCard = ({title, img }) => {
   return (
-    <div className="h-[150px] lg:h-[170px] relative bg-[#1c1c1c] rounded-2xl overflow-hidden shadow-lg hover:scale-[1.02] transition-all duration-300 border-2 border-neutral-800">
+    <div className="h-[160px] lg:h-[175px] relative bg-[#1c1c1c] rounded-2xl overflow-hidden shadow-lg hover:scale-[1.02] transition-all duration-300 border-2 border-neutral-800">
       {/* Image - centered */}
       <div className="flex items-center justify-center h-full w-full">
         <img src={img} alt={title} className="object-cover max-h-full max-w-full z-30" />
--- src/components/layouts/Categories.jsx
+++ src/components/layouts/Categories.jsx
@@ -5,9 +5,9 @@ import Container from "../Container";
 import CategoryOne from "../../assets/categoryOne.png";
 import CategoryTwo from "../../assets/categoryTwo.png";
 import CategoryThree from "../../assets/categoryThree.png";
-import serviceThree from "../../assets/serviceThree.jpg";
-import serviceFour from "../../assets/serviceFour.jpg";
-import serviceFive from "../../assets/serviceFive.jpg";
+import CategoryFour from "../../assets/categoryFour.png";
+import CategoryFive from "../../assets/categoryFive.png";
+import CategorySix from "../../assets/categorySix.png";
 import CategoriesCard from "../CategoriesCard";
 
 const Categories = () => {
@@ -15,9 +15,9 @@ const Categories = () => {
     { id: "1", title: "Luxury Car", img: CategoryOne },
     { id: "2", title: "Sports Car", img: CategoryTwo },
     { id: "3", title: "Sedan", img: CategoryThree },
-    { id: "4", title: "SUV", img: serviceThree },
-    { id: "5", title: "MPV", img: serviceFour },
-    { id: "6", title: "Small Car", img: serviceFive },
+    { id: "4", title: "SUV", img: CategoryFour },
+    { id: "5", title: "Small Car", img: CategorySix },
+    { id: "6", title: "MPV", img: CategoryFive },
   ];
   return (
     <>
@@ -29,7 +29,7 @@ const Categories = () => {
           Explore Our <span className="text-mainColor">Categories</span>
         </h3>
         {/* Categories Grid */}
-        <Container className={"mt-10 px-3"} >
+        <Container className={"mt-10 px-3"}>
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
             {category.map((item) => (
               <CategoriesCard
