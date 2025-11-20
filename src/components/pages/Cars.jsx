d0087839142b41b79cc8b2a13d6af43ee6bd880c
Twahidul Islam <twahidulislam2005@gmail.com>
2025-11-20 23:59:40 +0600

    add some

--- src/components/pages/Cars.jsx
+++ src/components/pages/Cars.jsx
@@ -172,7 +172,7 @@ const Cars = () => {
               }`}
             >
               {/* Search */}
-              <div className="bg-mainColor rounded-t-3xl px-6 py-4 sticky top-0 z-10">
+              <div className="bg-mainColor rounded-t-3xl px-6 py-4 sticky top-0 z-11">
                 <div className="relative max-w-md mx-auto">
                   <input
                     type="text"
